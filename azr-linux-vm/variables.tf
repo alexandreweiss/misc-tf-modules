5cfc973072ad02b3dded227454ea9b2cf22dc951
Alex Weiss <alexandreweiss@hotmail.com>
2022-12-01 11:33:11 +0100

    removed default for ssh public key

--- azr-linux-vm/variables.tf
+++ azr-linux-vm/variables.tf
@@ -26,7 +26,7 @@ variable "vm_size" {
 }
 
 variable "admin_ssh_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBsUy8OllCkhpOU4FplN1b7ypawC/8QM++3gb9EbqZHCJnJdTNhk/0QZVvGsPvWeSazsShgX2TdEMMdDFscWDdAfnoB+hyjhFyWaOfKXFdzafib3HrO0rGUPqW42V6d0N2V5rh23ZFZGX5Bp75KEFnrFgGY1axCebvMvStGzXXffole1sCt0SKbvFptc/MT/ZVSqT0i0ugS0dVXsb4kuo4qnNRUAqvunljDL5oS3ZT7bQtjAvcw+IyYF6Ka9pGc4EuNaYZ2YuaxMyMOKYoMq4Qz8Qk5oF34ATGCPC0SdAgtAByNblbYeB6s+ueWUwSEcKOfIKjl9lxJasCRBRkjl7zp non-prod-test"
+  description = "SSH public key to access deployed VM"
 }
 
 variable "index_number" {
