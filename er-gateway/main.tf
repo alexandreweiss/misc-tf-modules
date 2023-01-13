cbda664ef1503b561e85aa1d8a3d9cf01b0e5a68
Alex Weiss <alexandreweiss@hotmail.com>
2023-01-13 18:07:15 +0100

    added er gw module

---
+++ er-gateway/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_virtual_network_gateway" "er-gw" {
+  location            = var.location
+  name                = var.gateway_name
+  resource_group_name = var.resource_group_name
+  sku                 = var.gw_sku
+  type                = "ExpressRoute"
+  ip_configuration {
+    public_ip_address_id = azurerm_public_ip.pip.id
+    subnet_id            = var.gw_subnet_id
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "er-gw-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  allocation_method = "Dynamic"
+}
