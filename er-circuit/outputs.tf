35eecd4d18a903df645c0e92279f0c22bfb947a5
Alex Weiss <alexandreweiss@hotmail.com>
2023-09-11 18:17:45 +0200

    added circuit id

--- er-circuit/outputs.tf
+++ er-circuit/outputs.tf
@@ -8,3 +8,8 @@ output "circuit_name" {
   description = "Circuit name value"
   value       = azurerm_express_route_circuit.er-circuit.name
 }
+
+output "circuit_id" {
+  description = "ID of the circuit"
+  value       = azurerm_express_route_circuit.er-circuit.id
+}
