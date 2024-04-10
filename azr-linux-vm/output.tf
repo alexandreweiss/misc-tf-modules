96113ef0035ac2375241aec060dab6d63ae98729
Alex Weiss <alexandreweiss@hotmail.com>
2024-04-10 07:13:12 +0200

    nsg_id export

--- azr-linux-vm/output.tf
+++ azr-linux-vm/output.tf
@@ -1,4 +1,9 @@
 output "vm_private_ip" {
-  value = azurerm_linux_virtual_machine.vm.private_ip_address
+  value       = azurerm_linux_virtual_machine.vm.private_ip_address
   description = "Private IP address of the VM"
+}
+
+output "nsg_id" {
+  value       = var.enable_public_ip ? azurerm_network_security_group.nsg[0].id : null
+  description = "ID of the NSG"
 }
