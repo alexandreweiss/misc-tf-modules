51bb67638afcef12a69783f2ebf060c8caaecbeb
Alex Weiss <alexandreweiss@hotmail.com>
2025-01-12 11:40:00 +0100

    added export for public IPs

--- vpn-gateway/outputs.tf
+++ vpn-gateway/outputs.tf
@@ -2,3 +2,8 @@ output "vpn_gateway" {
   description = "VPN GW Object"
   value       = azurerm_virtual_network_gateway.vpn_gw
 }
+
+output "vpn_gateway_public_ip" {
+  description = "VPN GW Public IP"
+  value       = azurerm_public_ip.pip
+}
