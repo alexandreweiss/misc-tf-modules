e1526f8f039c959cae9e8f9800db9b8e38a23ddc
Alex Weiss <alexandreweiss@hotmail.com>
2025-01-12 01:08:39 +0100

    Added variable for the VPN Gateway name

--- vpn-gateway/main.tf
+++ vpn-gateway/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_public_ip" "pip" {
 }
 
 resource "azurerm_virtual_network_gateway" "vpn_gw" {
-  name                = "vpn-gw"
+  name                = var.gateway_name
   location            = var.location
   resource_group_name = var.resource_group_name
   type                = "Vpn"
