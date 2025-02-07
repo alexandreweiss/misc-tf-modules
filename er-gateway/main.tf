879e0ee68d7674c4add38e981c4b5cd5e6067af5
Alex Weiss <alexandreweiss@hotmail.com>
2025-02-08 00:48:12 +0100

    update on remote vnet traffic

--- er-gateway/main.tf
+++ er-gateway/main.tf
@@ -1,9 +1,11 @@
 resource "azurerm_virtual_network_gateway" "er_gw" {
-  location            = var.location
-  name                = var.gateway_name
-  resource_group_name = var.resource_group_name
-  sku                 = var.gw_sku
-  type                = "ExpressRoute"
+  location                    = var.location
+  name                        = var.gateway_name
+  resource_group_name         = var.resource_group_name
+  sku                         = var.gw_sku
+  type                        = "ExpressRoute"
+  remote_vnet_traffic_enabled = true
+  virtual_wan_traffic_enabled = true
   ip_configuration {
     public_ip_address_id = azurerm_public_ip.pip.id
     subnet_id            = var.gw_subnet_id
